22969ce130c32c832335ee69d1f79951b6649d96
Maksym Duzhyi <max5436d@gmail.com>
2023-10-13 21:23:35 +0300

    My First Commit

---
+++ ALevel DZ2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] massiv = { -40, 50, 60, 150, 200, 4, -120, 5 };
+        for(int i = 0; i < massiv.Length; i++)
+        {
+            if (massiv[i] > -100 && massiv[i] < 100)
+            {
+                Console.WriteLine(massiv[i]);
+            }
+        }
+    }
+}
+
+    
