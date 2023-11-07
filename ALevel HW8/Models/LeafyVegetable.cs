e84a43d50f708a2625a4ba88318ca59c079e36b3
Maksym Duzhyi <max5436d@gmail.com>
2023-11-07 18:57:43 +0200

    Home Work 8

---
+++ ALevel HW8/Models/LeafyVegetable.cs
@@ -0,0 +1,14 @@
+﻿namespace ALevel_HW8.Models
+{
+    public class LeafyVegetable : Vegetables
+    {
+        public string Color { get; set; }
+        public string Texture { get; set; }
+
+        public override string GetVegetableInfo()
+        {
+            return $"{base.GetVegetableInfo()}, Color: {Color}, Texture: {Texture}";
+        }
+    }
+
+}
