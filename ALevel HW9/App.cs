69edd406d7d58622aff07f4f40d7f8494fdf7e70
Maksym Duzhyi <max5436d@gmail.com>
2023-11-17 18:32:28 +0200

    Home Work 9

---
+++ ALevel HW9/App.cs
@@ -0,0 +1,35 @@
+﻿namespace ALevel_HW9
+{
+    public class App
+    {
+        public static void Run()
+        {
+            Random random = new Random();
+
+            for (int counter = 0; counter < 100; counter++)
+            {
+                int randomAction = random.Next(1, 4);
+
+                try
+                {
+                    switch (randomAction)
+                    {
+                        case 1:
+                            Actions.StartMethod();
+                            break;
+                        case 2:
+                            Actions.ThrowBusinessException();
+                            break;
+                        case 3:
+                            Actions.ThrowException();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log("Error", $"Action failed by reason: {ex}");
+                }
+            }
+        }
+    }
+}
---
+++ ALevel HW9/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALevel_HW9
+{
+    public class LogEntry
+    {
+        public string LogTime { get; set; }
+        public string LogType { get; set; }
+        public string Message { get; set; }
+    }
+}
