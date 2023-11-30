1d38a4662be7064ccb3c2c061967ba6afa65c002
Maksym Duzhyi <max5436d@gmail.com>
2023-11-30 15:41:58 +0200

    Home Work 13

---
+++ ALevel HW13/Program.cs
@@ -0,0 +1,20 @@
+﻿using ALevel_HW13;
+using static ALevel_HW13.FirstClass;
+using static ALevel_HW13.SecondClass;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FirstClass firstClass = new FirstClass();
+        SecondClass secondClass = new SecondClass();
+
+        FirstDelegate firstDelegate = Show;
+
+        ResultDelegate resultDelegate = secondClass.Calc(firstClass.Multiply, 10, 2);
+
+        firstDelegate(resultDelegate(5));
+    }
+
+    public static void Show(bool result) => Console.WriteLine(result);
+}
