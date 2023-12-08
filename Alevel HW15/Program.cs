0a82b7051a636b03ebf2fb815686f8836615bd98
Maksym Duzhyi <max5436d@gmail.com>
2023-12-08 18:58:20 +0200

    Home Work 15

---
+++ Alevel HW15/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static async Task Main()
+    {
+        string result = await ReadAndConcatenateAsync();
+
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> ReadFileAsync(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+
+    static async Task<string> ReadAndConcatenateAsync()
+    {
+        Task<string> helloTask = ReadFileAsync("Hello.txt");
+        Task<string> worldTask = ReadFileAsync("World.txt");
+
+        await Task.WhenAll(helloTask, worldTask);
+
+        string concatenatedResult = $"{await helloTask} {await worldTask}";
+
+        return concatenatedResult;
+    }
+}
