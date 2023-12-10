74ebb6b5829301a5ea0186f0a9aa8d5305cb1b21
Maksym Duzhyi <max5436d@gmail.com>
2023-12-11 01:34:37 +0200

    Home Work 16

---
+++ ALevel HW16/Program.cs
@@ -0,0 +1,25 @@
+﻿using ALevel_HW16;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MessageBox messageBox = new MessageBox();
+
+        messageBox.WindowClosed += (sender, args) =>
+        {
+            if (args.state == State.Ok)
+            {
+                Console.WriteLine("Operation confirmed.");
+            }
+            else
+            {
+                Console.WriteLine("Operation rejected.");
+            }
+        };
+
+        messageBox.Open();
+
+        Console.ReadLine();
+    }
+}
