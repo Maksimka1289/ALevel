f9b07ed74a21302c50409218ffccad1fbe38603e
Maksym Duzhyi <max5436d@gmail.com>
2023-12-12 17:19:55 +0200

    Home Work 18

---
+++ ALevel HW17/Logger.cs
@@ -0,0 +1,53 @@
+﻿namespace ALevel_HW17
+{
+    public class Logger
+    {
+        private readonly List<string> logEntries = new List<string>();
+        private int logCount = 0;
+        private readonly int backupThreshold;
+        private readonly string backupFolderPath;
+
+        public event EventHandler BackupRequired;
+
+        public Logger(int backupThreshold, string backupFolderPath)
+        {
+            this.backupThreshold = backupThreshold;
+            this.backupFolderPath = backupFolderPath;
+
+            if (!Directory.Exists(backupFolderPath))
+            {
+                Directory.CreateDirectory(backupFolderPath);
+            }
+        }
+
+        public async Task LogAsync(string message)
+        {
+            logEntries.Add($"{DateTime.Now:s} - {message}");
+            logCount++;
+
+            if (logCount >= backupThreshold)
+            {
+                OnBackupRequired();
+
+                logCount = 0;
+            }
+        }
+
+        private void OnBackupRequired()
+        {
+            BackupRequired?.Invoke(this, EventArgs.Empty);
+        }
+
+        public async Task PerformBackupAsync()
+        {
+            string backupFileName = $"{DateTime.Now:yyyyMMddHHmmss}.log";
+            string backupFilePath = Path.Combine(backupFolderPath, backupFileName);
+
+            await File.WriteAllLinesAsync(backupFilePath, logEntries);
+
+            Console.WriteLine($"Backup completed: {backupFilePath}");
+
+            logEntries.Clear();
+        }
+    }
+}
---
+++ ALevel HW17/Program.cs
@@ -0,0 +1,27 @@
+﻿using ALevel_HW17;
+using Newtonsoft.Json;
+internal class Program
+{
+    static async Task Main()
+    {
+        var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+
+        var logger = new Logger(config.BackupThreshold, config.BackupFolderPath);
+
+        logger.BackupRequired += async (sender, args) => await logger.PerformBackupAsync();
+
+        var task1 = Task.Run(async () => await LogEntriesAsync(logger, 50));
+        var task2 = Task.Run(async () => await LogEntriesAsync(logger, 50));
+
+        await Task.WhenAll(task1, task2);
+    }
+
+    static async Task LogEntriesAsync(Logger logger, int count)
+    {
+        for (int i = 1; i <= count; i++)
+        {
+            await logger.LogAsync($"Log entry {i}");
+            Thread.Sleep(50);
+        }
+    }
+}
