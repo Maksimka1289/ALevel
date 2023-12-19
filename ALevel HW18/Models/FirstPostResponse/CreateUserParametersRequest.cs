5aa167639726ec06f44dcd2417f1d02a5962ded8
Maksym Duzhyi <max5436d@gmail.com>
2023-12-19 15:11:57 +0200

    Home Work 18

---
+++ ALevel HW18/Models/FirstPostResponse/CreateUserParametersRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ALevel_HW18.Models
+{
+    internal sealed class CreateUserParametersRequest
+    {
+        public string Name { get; set; }
+
+        public string Job { get; set; }
+    }
+}
---
+++ ALevel HW18/Models/SecondGetResponse/ReqresPageGetTwoRequest.cs
@@ -0,0 +1,11 @@
+﻿using ALevel_HW18.Models.FirstGetResponse;
+using ALevel_HW18.Models.FiveGetResponse;
+
+namespace ALevel_HW18.Models.SecondGetResponse
+{
+    internal sealed class ReqresPageGetTwoRequest
+    {
+        public ReqresUserFiveRequest Data { get; set; }
+        public ReqresUserTwoRequest Support { get; set; }
+    }
+}
