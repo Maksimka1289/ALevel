7fa5071f00b536a0d0091beabbfef6a2bef6562f
Maksym Duzhyi <max5436d@gmail.com>
2024-01-09 19:54:15 +0200

    Home Work 21

---
+++ ALevel HW21/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ALevel_HW21
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Category_Name { get; set; }
+
+        public List<Pet> Pets { get; set; }
+
+        public List<Breed> Breeds { get; set; }
+    }
+}
---
+++ ALevel HW21/Program.cs
@@ -0,0 +1,7 @@
+﻿using ALevel_HW21;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+await using (AnimalsContext context = new AnimalsContentFactory().CreateDbContext(Array.Empty<string>()))
+{
+    context.Database.EnsureCreated();
+}
